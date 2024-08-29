72a68c79e937a2909f08e2368da98c177dd4b9c4
tax1116 <tax941116@gmail.com>
2024-08-29 12:32:28 +0900

    configures redisson-spring-boot-starter

--- concurrency/build.gradle.kts
+++ concurrency/build.gradle.kts
@@ -6,4 +6,17 @@ dependencies {
     implementation(libs.spring.boot.starter)
     implementation(libs.spring.boot.starter.web)
     implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.redisson)
+    implementation("io.netty:netty-resolver-dns-native-macos:4.1.107.Final:osx-x86_64") // 인텔 맥 DNS 설정
+}
+
+
+configurations.all {
+    resolutionStrategy {
+        eachDependency {
+            if (requested.group == "io.netty") {
+                useVersion("4.1.107.Final")
+            }
+        }
+    }
 }
