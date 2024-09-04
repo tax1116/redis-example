6ffaa36f70b19821356ac8d838e372729d9f2d01
tax1116 <tax941116@gmail.com>
2024-09-04 21:13:12 +0900

    [#7] Redis Lua 스크립트 예제 작성

--- concurrency/src/test/kotlin/kr/co/taek/dev/redis/example/concurrency/transaction/AtomicOperationServiceTests.kt
+++ concurrency/src/test/kotlin/kr/co/taek/dev/redis/example/concurrency/transaction/AtomicOperationServiceTests.kt
@@ -44,4 +44,31 @@ class AtomicOperationServiceTests {
         // then
         redisTemplate.opsForValue().get(key) shouldBe 2
     }
+
+    @DisplayName("2개의 스레드가 동시에 Redis에 접근하여 값을 증가시키더라도 Lua 스크립트에 의해 값이 각각 1씩 증가한다.")
+    @Test
+    fun increment_with_lua_script() {
+        // given
+        val key = "test"
+
+        redisTemplate.opsForValue().get(key)?.let {
+            redisTemplate.delete(key)
+        }
+
+        // when
+        val futures = mutableListOf<CompletableFuture<Void>>()
+        repeat(2) {
+            val future =
+                CompletableFuture.runAsync {
+                    atomicOperationService.incrementWithLuaScript(key)
+                }
+
+            futures.add(future)
+        }
+
+        futures.forEach { it.join() }
+
+        // then
+        redisTemplate.opsForValue().get(key) shouldBe 2
+    }
 }
